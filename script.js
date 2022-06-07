cdd672bb3cb6914dcca00ff40489566207d6ff4a
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-06-07 15:58:53 +0200

    move code parts to functions inflate and deflate

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let balloon = document.querySelector("p");
 
+function inflate() {
+    let balloonSize = Number(balloon.style.fontSize.replace("px", ""));
+    balloonSize *= 1.1;
+    balloonSize = balloonSize + "px";
+    balloon.style.fontSize = balloonSize;
+    console.log("It GROWS!");
+}
 
-window.addEventListener("keydown", event => { //Arrow keys are only triggered by onkeydown, not onkeypress.
+function deflate() {
+    let balloonSize = Number(balloon.style.fontSize.replace("px", ""));
+    balloonSize *= 0.9;
+    balloonSize = balloonSize + "px";
+    balloon.style.fontSize = balloonSize;
+    console.log("It SHRINKS!");
+}
 
-    if (event.key === "ArrowUp") {
-        event.preventDefault(); //prevents from default scrolling
-        let balloonSize = Number(balloon.style.fontSize.replace("px", ""));
-        balloonSize *= 1.1;
-        balloonSize = balloonSize + "px";
-        balloon.style.fontSize = balloonSize;
-        event.repeat = false; // how to stop event.repeat ?? if the key is pressed long?????
-        console.log("It GROWS!");
+
+window.addEventListener("keydown", event1 => { //Arrow keys are only triggered by onkeydown, not onkeypress.
+
+    if (event1.key === "ArrowUp") {
+        event1.preventDefault(); //prevents from default scrolling
+        event1.repeat = false; // how to stop event.repeat ?? if the key is pressed long?????
+        inflate();
         if (Number(balloon.style.fontSize.replace("px", "")) > 100) {
             balloon.innerText = "💥";
-            window.removeEventListener("keydown", event);
+            window.removeEventListener("keydown", event1);
         }
     }
 }
@@ -23,12 +35,8 @@ window.addEventListener("keydown", event => { //Arrow keys are only triggered by
     console.log(event.key);
     if (event.key === "ArrowDown") {
         event.preventDefault(); //prevents from default scrolling
-        let balloonSize = Number(balloon.style.fontSize.replace("px", ""));
-        balloonSize *= 0.9;
-        balloonSize = balloonSize + "px";
-        balloon.style.fontSize = balloonSize;
         event.repeat = false;
-        console.log("It SHRINKS!");
+        deflate();
 
     }
 }
